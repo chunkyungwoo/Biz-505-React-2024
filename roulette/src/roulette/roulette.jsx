d922e679d5304a40eabc699ea762e50a34debc2b
unknown <ckw2434@naver.com>
2024-05-17 15:42:40 +0900

    i

--- roulette/src/roulette/roulette.jsx
+++ roulette/src/roulette/roulette.jsx
@@ -23,31 +23,41 @@ const Roulette = () => {
   const [isRotating, setIsRotating] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(0);
   const [startingOptionIndex, setStartingOptionIndex] = useState(0);
+  const [hasSpun, setHasSpun] = useState(false);
 
-  const rotateTime = 8000; // 8초
+  const rotateTime = 3000;
 
   const startRotation = () => {
+    setHasSpun(true);
     const newStartingOptionIndex = Math.floor(
       Math.random() * categories.length
     );
     setStartingOptionIndex(newStartingOptionIndex);
 
     setIsRotating(true);
+    const newPrizeNumber = Math.floor(
+      Math.random() * categories.length
+    );
+    setPrizeNumber(newPrizeNumber);
     setTimeout(() => {
       setIsRotating(false);
-      const newPrizeNumber = Math.floor(
-        Math.random() * categories.length
-      );
-      setPrizeNumber(newPrizeNumber);
       setResult(categories[newPrizeNumber]);
     }, rotateTime);
   };
 
   useEffect(() => {
-    if (!isRotating) {
+    if (!isRotating && result !== null && hasSpun) {
+      alert(`Selected Option: ${result}`);
+      setHasSpun(false); // 알림 후 상태 초기화
+    }
+  }, [result, isRotating, hasSpun]);
+
+  // && prizeNumber !== null
+  useEffect(() => {
+    if (!isRotating && hasSpun) {
       setResult(categories[prizeNumber]);
     }
-  }, [prizeNumber, isRotating]);
+  }, [prizeNumber, isRotating, hasSpun]);
 
   return (
     <RouletteContainer>
@@ -62,13 +72,15 @@ const Roulette = () => {
         Spin
       </Button>
       <Wheel // Wheel 컴포넌트 사용
-        spinDuration={0.2}
+        spinDuration={rotateTime / 1000}
         startingOptionIndex={startingOptionIndex}
         mustStartSpinning={isRotating}
         prizeNumber={prizeNumber}
         data={categories.map((category) => ({ option: category }))}
-        onStopSpinning={() => {}}
-        key={startingOptionIndex}
+        onStopSpinning={() => {
+          setIsRotating(false);
+        }}
+        // key={startingOptionIndex}
       />
       {result && (
         <Typography variant="h6" gutterBottom>
