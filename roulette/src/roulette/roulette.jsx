b0af83a068f14518fd0fa2cfdffa20d268a70cb2
unknown <ckw2434@naver.com>
2024-05-22 15:41:28 +0900

    food

--- roulette/src/roulette/roulette.jsx
+++ roulette/src/roulette/roulette.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Button, Typography, Box } from "@mui/material";
+import {
+  Button,
+  Typography,
+  Box,
+  List,
+  ListItem,
+} from "@mui/material";
 import { styled } from "@mui/system";
 import { Wheel } from "react-custom-roulette";
 
@@ -30,6 +36,7 @@ const Roulette = () => {
   const [startingOptionIndex, setStartingOptionIndex] = useState(0);
   const [hasSpun, setHasSpun] = useState(false);
   const [timerId, setTimerId] = useState(null);
+  const [resultData, setResultData] = useState([]);
 
   const rotateTime = 8000;
 
@@ -76,6 +83,33 @@ const Roulette = () => {
   // };
 
   useEffect(() => {
+    if (result) {
+      console.log(`Fetching data for category: ${result}`);
+      fetch(`/react/getCategory/${result}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+        .then((res) => {
+          console.log("Response status:", res.status);
+          console.log("Response headers:", res.headers);
+          if (!res.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return res.json();
+        })
+        .then((newData) => {
+          console.log("Received data:", newData);
+          setResultData(newData);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, [result]);
+
+  useEffect(() => {
     if (!isRotating && result !== null && hasSpun) {
       alert(`Selected Option: ${result}`);
       setHasSpun(false); // 알림 후 상태 초기화
@@ -115,10 +149,34 @@ const Roulette = () => {
         // key={startingOptionIndex}
       />
       {result && (
-        <Typography variant="h6" gutterBottom>
-          {`Selected Option: ${result}`}
-        </Typography>
+        <>
+          <Typography variant="h6" gutterBottom>
+            {`Selected Option: ${result}`}
+          </Typography>
+          <Box sx={{ marginTop: "20px" }}>
+            <Typography variant="h5" gutterBottom>
+              결과 :
+            </Typography>
+            <List>
+              {resultData.length > 0 ? (
+                resultData.map((item, index) => (
+                  <ListItem key={index}>{item}</ListItem>
+                ))
+              ) : (
+                <ListItem>데이터 없음</ListItem>
+              )}
+            </List>
+          </Box>
+        </>
       )}
+      <Button
+        variant="contained"
+        onClick={startRotation}
+        disabled={isRotating}
+        sx={{ marginTop: "20px" }}
+      >
+        Spin Again
+      </Button>
     </RouletteContainer>
   );
 };
